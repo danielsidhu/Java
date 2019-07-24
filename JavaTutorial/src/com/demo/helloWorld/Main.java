7ed0e0e1920a111896d096b0b4d21f94a0c22cea
danielsidhu <40637004+danielsidhu@users.noreply.github.com>
2019-07-23 20:48:12 -0400

    Morgage Payment Calculator added

--- JavaTutorial/src/com/demo/helloWorld/Main.java
+++ JavaTutorial/src/com/demo/helloWorld/Main.java
@@ -4,9 +4,33 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+/*		Scanner scanner = new Scanner(System.in);
 		System.out.print("Age: ");
 		int age = scanner.nextInt();
-		System.out.println("You are " + age);
+		System.out.println("You are " + age);*/
+		
+		int percent = 100;
+		int months = 12;
+		double interestRate;
+		double morgage;
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("What is your principal? ");
+		int principal = scanner.nextInt();
+		System.out.print("What is your monthly interest rate? ");
+		double interest = scanner.nextDouble();
+		System.out.print("Number of payments? ");
+		int payments = scanner.nextInt();
+		
+		interestRate = ((interest/percent)/months);
+		
+		System.out.println("Interest Rate is " + interestRate);	
+		
+		morgage = principal*
+					(interestRate*Math.pow(1+interestRate, payments))
+						/((Math.pow(1+interestRate, payments))-1);
+		
+		System.out.println("Morgage is " + morgage);
+		
 	}
 }
