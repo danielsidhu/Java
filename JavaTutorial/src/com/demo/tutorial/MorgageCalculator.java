20a72ca2f8ec6c3f55ec106ec18ff2ab75155db0
danielsidhu <40637004+danielsidhu@users.noreply.github.com>
2019-07-25 20:58:01 -0400

    Morgage Calculator Error handling added

--- JavaTutorial/src/com/demo/tutorial/MorgageCalculator.java
+++ JavaTutorial/src/com/demo/tutorial/MorgageCalculator.java
@@ -3,36 +3,58 @@ package com.demo.tutorial;
 import java.util.Scanner;
 
 public class MorgageCalculator {
-	
+
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-/*		Scanner scanner = new Scanner(System.in);
-		System.out.print("Age: ");
-		int age = scanner.nextInt();
-		System.out.println("You are " + age);*/
-		
+
 		int percent = 100;
 		int months = 12;
-		double interestRate;
+		double interestRate = 0;
 		double morgage;
-		
+		int principal = 0;
+		double interest = 0;
+		int payments = 0;
+
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("What is your principal? ");
-		int principal = scanner.nextInt();
-		System.out.print("What is your monthly interest rate? ");
-		double interest = scanner.nextDouble();
-		System.out.print("Number of payments? ");
-		int payments = scanner.nextInt();
-		
-		interestRate = ((interest/percent)/months);
-		
-		System.out.println("Interest Rate is " + interestRate);	
-		
-		morgage = principal*
-					(interestRate*Math.pow(1+interestRate, payments))
-						/((Math.pow(1+interestRate, payments))-1);
-		
+
+		while (true) {
+			System.out.print("What is your principal? ");
+			principal = scanner.nextInt();
+
+			if (principal >= 1000 && principal <= 1_000_000) {
+				break;
+			} else {
+				System.out.println("Please enter a value between 1K ans 1M");
+			}
+		}
+
+		while (true) {
+			System.out.print("What is your monthly interest rate? ");
+			interest = scanner.nextDouble();
+
+			if (interest >= 0 && interest <= 30) {
+				interestRate = ((interest / percent) / months);
+				break;
+			} else {
+				System.out.println("Please enter a value between 0 and 30");
+			}
+		}
+
+		while (true) {
+			System.out.print("Number of payments? ");
+			payments = scanner.nextInt();
+
+			if (payments >= 0) {
+				break;
+			} else {
+				System.out.println("Please enter a positive value");
+			}
+		}
+
+		morgage = principal * (interestRate * Math.pow(1 + interestRate, payments))
+				/ ((Math.pow(1 + interestRate, payments)) - 1);
+
 		System.out.println("Morgage is " + morgage);
-		
+
 	}
 }
