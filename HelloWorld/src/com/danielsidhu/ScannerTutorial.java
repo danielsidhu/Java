89ff8cb8edd56eaf87ddd4ae1dffbd0bb6a8d491
danielsidhu <40637004+danielsidhu@users.noreply.github.com>
2019-08-09 21:32:29 -0400

    refactoring the morgage calculator

--- HelloWorld/src/com/danielsidhu/ScannerTutorial.java
+++ HelloWorld/src/com/danielsidhu/ScannerTutorial.java
@@ -6,31 +6,20 @@ public class ScannerTutorial {
 
 	public static void main(String[] args) {
 
-		int percent = 100;
-		int months = 12;
-		double interestRate;
-		double rate;
-		double principal;
-		int payments;
+//		int percent = 100;
+//		int months = 12;
+		double annualInterest = 0;
+//		double monthlyInterest = 0;
+		double principal = 0;
+		int years = 0;
+
 		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
 
 		while (true) {
-			System.out.print("Enter your monthly interest rate between 0 and 100 ");
-			interestRate = scanner.nextDouble();
-
-			if (interestRate >= 0 && interestRate <= 100) {
-				rate = (interestRate / percent) / months;
-				break;
-			} else {
-				System.out.println("Nope, try again");
-				continue;
-			}
-		}
-
-		while (true) {
 			System.out.print("Enter your principal between 1k and 1M ");
 			principal = scanner.nextDouble();
+
 			if (principal >= 1_000 && principal <= 1_000_000) {
 				break;
 			} else {
@@ -38,11 +27,13 @@ public class ScannerTutorial {
 				continue;
 			}
 		}
-
+		
 		while (true) {
-			System.out.print("Enter your monthly payment ");
-			payments = scanner.nextInt();
-			if (payments >= 0) {
+			System.out.print("Enter your monthly interest rate between 0 and 100 ");
+			annualInterest = scanner.nextDouble();
+
+			if (annualInterest >= 0 && annualInterest <= 100) {
+//				monthlyInterest = (annualInterest / percent) / months;
 				break;
 			} else {
 				System.out.println("Nope, try again");
@@ -50,7 +41,37 @@ public class ScannerTutorial {
 			}
 		}
 
-		double morgage = principal * (rate * Math.pow((1 + rate), payments) / ((Math.pow((1 + rate), payments) - 1)));
-		System.out.println("Your Morgage is " + morgage);
+		while (true) {
+			System.out.print("Enter your yearly payment between 1 and 30");
+			years = scanner.nextInt();
+
+			if (years >= 0 && years <= 30) {
+//				payments = years * months;
+				break;
+			} else {
+				System.out.println("Nope, try again");
+				continue;
+			}
+		}
+		
+//		double morgage = principal * (monthlyInterest * Math.pow((1 + monthlyInterest), payments ) / ((Math.pow((1 + monthlyInterest), payments) - 1)));
+//		System.out.println("Your Morgage is " + morgage);
+		
+		double mortgage = calculateMortgage(principal, annualInterest, years);
+		System.out.println("Your Morgage is " + mortgage);
+	}
+
+	public static double calculateMortgage(double principal, double annualInterest, int years) {
+		
+		int months = 12;
+		int percent = 100;
+		
+		double monthlyInterest = (annualInterest / percent) / months;
+		int payments = years  * months ;
+		
+		double mortgage = principal * (monthlyInterest * Math.pow((1 + monthlyInterest), payments ) / ((Math.pow((1 + monthlyInterest), payments) - 1)));
+//		System.out.println("Your Morgage is " + morgage);
+		
+		return mortgage;
 	}
 }
--- JavaTutorial/src/com/demo/tutorial/MorgageCalculator.java
+++ JavaTutorial/src/com/demo/tutorial/MorgageCalculator.java
@@ -7,16 +7,16 @@ public class MorgageCalculator {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 
-		int percent = 100;
-		int months = 12;
-		double interestRate = 0;
-		double morgage;
 		int principal = 0;
-		double interest = 0;
 		int payments = 0;
+		double interestRate = 0;
 
 		Scanner scanner = new Scanner(System.in);
 
+//		int principal = (int) readNumber("Principal: ", 1_000, 1_000_000);
+//		double interestRate = (double) readNumber("Annual Interest Rate: ", 1, 30);
+////		int payments = (int) readNumber("Period (Months): ", 1, 1000);
+		
 		while (true) {
 			System.out.print("What is your principal? ");
 			principal = scanner.nextInt();
@@ -33,7 +33,6 @@ public class MorgageCalculator {
 			interest = scanner.nextDouble();
 
 			if (interest >= 0 && interest <= 30) {
-				interestRate = ((interest / percent) / months);
 				break;
 			} else {
 				System.out.println("Please enter a value between 0 and 30");
@@ -44,17 +43,46 @@ public class MorgageCalculator {
 			System.out.print("Number of payments? ");
 			payments = scanner.nextInt();
 
-			if (payments >= 0) {
+			if (payments >= 1 && payments <= 30) {
 				break;
 			} else {
 				System.out.println("Please enter a positive value");
 			}
 		}
 
-		morgage = principal * (interestRate * Math.pow(1 + interestRate, payments))
-				/ ((Math.pow(1 + interestRate, payments)) - 1);
-
+//		double morgage = calculateMortgage(principal, interestRate);
 		System.out.println("Morgage is " + morgage);
 
+	}
+	
+	@SuppressWarnings("resource")
+	public static double readNumber(String prompt, double min, double max) {
+		Scanner scanner = new Scanner(System.in);
+		double value;
+		
+		while (true) {
+			System.out.print(prompt);
+			value = scanner.nextDouble();
+
+			if (value >= min && value <= max) {
+				break;
+			} else {
+				System.out.println("Please enter a value between" + min + " and " + max);
+			}
+		}
+		return value;
+	}
+
+	public static double calculateMortgage(int principal, double interestRate) {
+
+		double interest = 0;
+		int percent = 100;
+		int months = 12;
+		int payments = 0;
+		interestRate = ((interest / percent) / months);
+
+		double morgage = principal * (interestRate * Math.pow(1 + interestRate, payments))
+				/ ((Math.pow(1 + interestRate, payments)) - 1);
+		return morgage;
 	}
 }
