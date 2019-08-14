0c76062f7b596beff686e31a773c057380b8e44c
danielsidhu <40637004+danielsidhu@users.noreply.github.com>
2019-08-13 20:44:33 -0400

    refactoring employee wage calculator

--- JavaTutorial/src/com/demo/tutorial/Employee.java
+++ JavaTutorial/src/com/demo/tutorial/Employee.java
@@ -3,12 +3,28 @@ package com.demo.tutorial;
 public class Employee {
 	private int baseSalary;
 	private int hourlyRate;
+	
+	public static int numberOfEmployees;
 
+	public Employee(int baseSalary) {
+		this(baseSalary, 0);
+	}
+	
+	public Employee(int baseSalary, int hourlyRate) {
+		setBaseSalary(baseSalary);
+		setHourlyRate(hourlyRate);
+		numberOfEmployees++;
+	}
+	
+	public static void printNumberOfEmployees() {
+		System.out.println(numberOfEmployees);
+	}
+	
 	public int calculateWage(int extraHours) {
 		return baseSalary + (hourlyRate * extraHours);
 	}
 
-	public void setBaseSalary(int baseSalary) {
+	private void setBaseSalary(int baseSalary) {
 		if (baseSalary <= 0) {
 			throw new IllegalArgumentException("Cannot enter in a negative base salary");
 		} else
@@ -19,7 +35,7 @@ public class Employee {
 		return baseSalary;
 	}
 
-	public void setHourlyRate(int hourlyRate) {
+	private void setHourlyRate(int hourlyRate) {
 		if (hourlyRate <= 0) {
 			throw new IllegalArgumentException("Cannot enter in a negative hourly rate");
 		} else
--- JavaTutorial/src/com/demo/tutorial/Main.java
+++ JavaTutorial/src/com/demo/tutorial/Main.java
@@ -4,20 +4,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		//here we call the TextBox Class and use it
-		TextBox textBox1 = new TextBox();
-		textBox1.setText("Box 1");
-		System.out.println(textBox1.text);
+//		TextBox textBox1 = new TextBox();
+//		textBox1.setText("Box 1");
+//		System.out.println(textBox1.text);
+//		
+//		TextBox textBox2 = new TextBox();
+//		textBox2.setText("Box 2");
+//		System.out.println(textBox2.text);
 		
-		TextBox textBox2 = new TextBox();
-		textBox2.setText("Box 2");
-		System.out.println(textBox2.text);
-		
-		Employee employee = new Employee();
-		employee.setBaseSalary(-1);
-		employee.getBaseSalary();
-		employee.setHourlyRate(20);
+		Employee employee = new Employee(500, 20);
+//		employee.setBaseSalary(50_000);
+//		employee.setHourlyRate(20);
 		int wage = employee.calculateWage(5);
 		System.out.println(wage);
+		Employee.printNumberOfEmployees();
+		
+//		Browser browser = new Browser();
 	}
 
 }
