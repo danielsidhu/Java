7d2b5938668aa1c9785dd2f9c24e6dc58861c55b
danielsidhu <40637004+danielsidhu@users.noreply.github.com>
2019-09-04 20:26:06 -0400

    adding array list practice

--- HelloWorld/src/com/danielsidhu/Main.java
+++ HelloWorld/src/com/danielsidhu/Main.java
@@ -1,14 +1,27 @@
 package com.danielsidhu;
 
+import java.awt.List;
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		Users user = new Users();
-		user.firstName = "Daniel";
-		user.lastName = "Sidhu";
+		Users me = new Users();
+		me.firstName = "Daniel";
+		me.lastName = "Sidhu";
 		
-		System.out.println(user.getFullName());
+		Users you = new Users();
+		you.firstName = "Sherry";
+		you.lastName = "Hurtado";
+		
+		
+		ArrayList<Users> users = new ArrayList<Users>();
+		users.add(me);
+		users.add(you);
+			
+		System.out.println(users.get(1).getFullName());
+		}
 	}
 
-}
+
